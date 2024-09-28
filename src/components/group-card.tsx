213b24ea23fcc67cf0353a7fdce8934f0e342e99
Dagadev <gacub20ru@gmail.com>
2024-09-27 22:19:25 -0600

    Add GroupCard component

--- src/components/group-card.tsx
+++ src/components/group-card.tsx
@@ -1,5 +1,11 @@
-export default function GroupCard () {
+export default function GroupCard ({ group }: { group: LogGroup }) {
   return (
-    <article></article>
+    <article className='bg-primary rounded-md shadow-md py-2 px-3'>
+      <header className='flex gap-x-2 justify-between'>
+        <h2 className='font-bold'>{group.name}</h2>
+        <span className='font-semibold text-accent'>{group.logsAmount}</span>
+      </header>
+      {group.description !== undefined && <p className='text-secondary-text text-sm mt-2'>{group.description}</p>}
+    </article>
   )
 }
