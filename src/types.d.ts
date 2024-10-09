2a918cdb6a57d6fb99caec4e7845cb750a766b25
Dagadev <gacub20ru@gmail.com>
2024-10-09 13:09:15 -0600

    Add Credentials and CreateUserData interfaces

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,10 @@
 export type AddType = 'log' | 'group'
+
+export interface Credentials {
+  email: string
+  password: string
+}
+
+export interface CreateUserData extends Credentials {
+  name: string
+}
