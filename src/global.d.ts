2260c116adecec2b0abd2751a20de173f3ce33b2
Dagadev <gacub20ru@gmail.com>
2024-10-09 13:10:15 -0600

    Add User interface & Replace undefined by null props

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,18 +2,26 @@ declare interface SVGIconComponent extends React.ComponentProps<'svg'> {
   iconStyle?: 'fill' | 'outline'
 }
 
+declare interface User extends import('./types').CreateUserData {
+  id: string
+  createdAt: string
+  updatedAt: string
+}
+
 declare interface Log {
   id: string
-  title: string
+  name: string
+  content: string | null
   eventDate: string
-  groupId?: string
-  description?: string
+  group: LogGroup | null
+  createdAt: string
+  updatedAt: string
 }
 
 declare interface LogGroup {
   id: string
   name: string
-  description?: string
+  description: string | null
   createdAt: string
   logsAmount: number
 }
