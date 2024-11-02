34f031afd421261a64d54c3b428e6b5ab6fa0374
gacub20ru <gacub20ru@gmail.com>
2024-11-02 19:52:14 +0000

    Add console log

--- src/lib/client/index.ts
+++ src/lib/client/index.ts
@@ -4,6 +4,7 @@ export function setToken (name: string, value: string) {
 }
 
 export function getToken () {
+  console.log(document.cookie.split(';').find(s => s.includes('token=')))
   const token = document.cookie.split(';').find(s => s.includes('token='))?.replace('token=', '')
   console.log('getToken: ', { token })
   return token ?? ''
