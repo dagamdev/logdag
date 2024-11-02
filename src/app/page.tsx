f74c889d3cd172fe869eed8d0c38d5113484c2b2
gacub20ru <gacub20ru@gmail.com>
2024-11-02 04:29:25 +0000

    Get current onli fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import LogCard from '@/components/log-card'
+import { getToken } from '@/lib/client'
 import api from '@/lib/client/api'
 import { useLogsStore } from '@/stores/logs'
+import { API_URL } from '@/utils/constants'
 import { useEffect } from 'react'
 
 export default function Home () {
@@ -16,8 +18,14 @@ export default function Home () {
     }).catch(console.error)
 
     api.current().then(res => {
-      console.log(res.data)
+      console.log('with axios: ', res.data)
     }).catch(console.error)
+
+    fetch(`${API_URL}auth/current`, {
+      headers: {
+        authorization: `Bearer ${getToken()}`
+      }
+    }).then(async res => res.json()).then(data => { console.log('onli fetch: ', data) }).catch(console.error)
   }, [setLogs])
 
   return (
